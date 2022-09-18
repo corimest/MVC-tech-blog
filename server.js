51ec37211b71da6885c559f94add1b6f8d9fa989
Corinne Mestemacher <corimest@Corinnes-MacBook-Air.local>
2022-09-18 10:07:28 -0500

    login and logout functionality

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const { appendFile } = require('fs');
 const path = require('path'); 
 const express = require('express');
+const session = require('express-session'); 
 const app = express();
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store); 
 const routes = require('./controllers/')
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 
 const PORT = process.env.PORT || 3001;
 
